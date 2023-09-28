e93e08d47745dd5f1e9adbe36eef6f8d17d62545
Onur Cetin <onucetin003@gmail.com>
2023-09-28 14:12:45 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:feedapp/pages/splash1.dart';
 import 'package:flutter/material.dart';
 
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner:false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 145, 104, 216)),
         useMaterial3: true,
       ),
       home: SplashScreen(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,14 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key});
+class LoginPage extends StatefulWidget {
+ LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late FirebaseAuth auth;
+  String _email="";
+  String _password="";
+
+  @override
+  void initState() {
+    super.initState();
+    auth = FirebaseAuth.instance;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FlutterLogin(
-        title: ' En & Joy',
+        title: 'En & Joy',
         onLogin: _login,
         onSignup: _signup,
         onSubmitAnimationCompleted: () {
@@ -20,23 +36,45 @@ class LoginPage extends StatelessWidget {
   }
 
   Future<String?> _login(LoginData data) async {
+    createUserandEmail();
     // Giriş yapma işlemleri burada gerçekleştirilir.
     // Kullanıcı adı ve şifreyi `data.name` ve `data.password` ile alabilirsiniz.
     // Başarılı bir şekilde giriş yapılırsa `null` dönmelisiniz, aksi halde bir hata mesajı dönebilirsiniz.
     return null;
   }
 
-Future<String?> _signup(SignupData data) async {
-  // Kayıt olma işlemleri burada gerçekleştirilir.
-  // Kullanıcı adı ve şifreyi `data.name` ve `data.password` ile alabilirsiniz.
-  // Başarılı bir şekilde kayıt olunursa `null` dönmelisiniz, aksi halde bir hata mesajı dönebilirsiniz.
-  return null;
-}
+  Future<String?> _signup(SignupData data) async {
+    loginUserandEmail();
+    // Kayıt olma işlemleri burada gerçekleştirilir.
+    // Kullanıcı adı ve şifreyi `data.name` ve `data.password` ile alabilirsiniz.
+    // Başarılı bir şekilde kayıt olunursa `null` dönmelisiniz, aksi halde bir hata mesajı dönebilirsiniz.
+    return null;
+  }
 
   Future<String?> _recoverPassword(String name) async {
     // Şifre sıfırlama işlemleri burada gerçekleştirilir.
     // Kullanıcı adını `name` ile alabilirsiniz.
     // Şifre sıfırlama işlemi başarılı olursa `null` dönmelisiniz, aksi halde bir hata mesajı dönebilirsiniz.
     return null;
+  }
+  void createUserandEmail()async{
+  try{
+  var _userCrential= auth.createUserWithEmailAndPassword(email: _email, password: _password);
+
+  debugPrint(_userCrential.toString());
+  }catch(e){
+    debugPrint(e.toString());
+  }
+
+}
+  void loginUserandEmail()async{
+    try{
+      var _userCrential=await auth.signInWithEmailAndPassword(email: _email, password: _password);
+      debugPrint(_userCrential.toString());
+    }catch(e){
+      debugPrint(e.toString());
+
+    }
+
   }
 }
